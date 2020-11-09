5383aa5d312ad1df53e76053f163bb90e1a0a9a5
AyushRawat-17 <ayushrawatkv@gmail.com>
2020-11-09 10:45:32 +0530

    Webserver Program change

--- webserver.py
+++ webserver.py
@@ -3,17 +3,15 @@ import subprocess as sp
 import os
 
 def install():
-    output=sp.getstatusoutput('dnf install httpd -y')
-    if output[0]== 0:
-        print("\t\t\t\t\t   HTTPD Installed Successfully")
-    else:
-        print("\t\t\t\t\t   Failed")
+    os.system('dnf install httpd -y')
+    print("\t\t\t\t\t   HTTPD Installed Successfully")
 
 def configure():
     file=input("Enter File with path: ")
     os.system("cp {} /var/www/html".format(file))
     os.system("systemctl start httpd")
-    print("\t\t\t\t\t   Webserver Configured")
+    print("\t\t\t\t\t   Webserver Configured\n\n")
+    print("Now, Open Web Browser and type ip-address.....")
 
 def stop():
     os.system("systemctl stop httpd")
@@ -29,7 +27,7 @@ def menu():
             """)
        os.system('tput setaf 6')
        print("""
-                                                      Press '1': Install HTTPD
+                                                      Press '1': Install Webserver
                                                       Press '2': Configure Webserver 
                                                       Press '3': Stop Webserver
                                                       Press '0': Exit
